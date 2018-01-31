65fc5729a8cec219fe2f27796769636985a4e0c5
Rasmus Akerstrom <rakerstroms@gmail.com>
2018-01-31 10:32:59 +0100

    sdfsf

---
+++ src/main/java/com/littlemixrecipes/littlemix/LittlemixApplication.java
@@ -0,0 +1,12 @@
+package com.littlemixrecipes.littlemix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlemixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlemixApplication.class, args);
+	}
+}
---
+++ src/test/java/com/littlemixrecipes/littlemix/LittlemixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.littlemixrecipes.littlemix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LittlemixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
