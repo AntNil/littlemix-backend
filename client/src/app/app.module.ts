62e0ce015021dafb06f4b7858d3a9903aeb61e8a
M3832 <markus3832@gmail.com>
2018-01-31 13:39:07 +0100

    markus fix

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MarkustestComponent } from './markustest/markustest.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MarkustestComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
