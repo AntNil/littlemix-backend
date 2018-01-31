232dd94fe63a42ff3184eb90fc3a287c0d8145a2
Rasmus Akerstrom <rakerstroms@gmail.com>
2018-01-31 14:21:51 +0100

    ts

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+hello: boolean = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  bla(){
+    this.hello = false;
   }
 
 }
