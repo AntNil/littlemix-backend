0fa0718c150a6843eaa78c7e19a347d3e6ec163a
SofHay <sofhay@live.se>
2018-02-01 14:13:38 +0100

    update

--- client/src/app/recipe/recipe.component.ts
+++ client/src/app/recipe/recipe.component.ts
@@ -14,6 +14,7 @@ import {Ingredient} from "../models/ingredient.modal";
 
 export class RecipeComponent implements OnInit {
   @Input() rating: number;
+  @Input() submitComment: number;
   @Input() itemId: number;
   @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
 
@@ -30,6 +31,8 @@ export class RecipeComponent implements OnInit {
       new Ingredient('Bacon', '2 slices')
     ];
     this.recipe = new Recipe;
+    this.recipe.imgURL = 'https://d1doqjmisr497k.cloudfront.net/-/media/schwartz/recipes/800x800/bacon_sandwich_800.ashx?vd=20160802T004146Z&ir=1&width=600&height=600&crop=auto&quality=75&speed=0&hash=E34ACE89265017EAE6FA80D499960D876B388F200';
+    this.recipe.ingredient
     this.recipe.title = 'Title';
     this.recipe.description = 'Work, work, work!';
 
@@ -46,6 +49,12 @@ export class RecipeComponent implements OnInit {
       itemId: this.itemId,
       rating: rating
     });
+
+  }
+  onClick(submitComment: number): void {
+    this.submitComment = submitComment;
+    console.log("yay")
+
 
   }
 }
