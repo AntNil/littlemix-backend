91653b119b9bc2dd5c25ea559e579449bad9147d
M3832 <markus3832@gmail.com>
2018-02-01 10:45:11 +0100

    Routing fix

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginModalComponent} from "./authentication/login-modal/login-modal.component";
 
 const appRoutes: Routes = [
-<<<<<<< HEAD
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'login', component: LoginModalComponent}
-=======
   {path: '', redirectTo: '', pathMatch: 'full'}
->>>>>>> 5d36c2191ccf5c8d1e9d98d4602c84770bfc7fe0
 
 
 ];
