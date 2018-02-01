6c391c27b9cb12abeb62f780b5f5eaa5bf9f634a
M3832 <markus3832@gmail.com>
2018-02-01 14:57:33 +0100

    Updated recipe model

--- client/src/app/models/recipe.modal.ts
+++ client/src/app/models/recipe.modal.ts
@@ -2,16 +2,22 @@ import {Ingredient} from "./ingredient.modal";
 export class Recipe {
 
   id: number;
+  userId: number;
   title: string;
+  category: string;
   imgURL: string;
   ingredient: Ingredient[];
   description: string;
+  instruction: string;
 
   constructor() {
     this.id = 1;
+    this.userId = 2;
     this.title = "Pancakes";
     this.imgURL = "https://i.ytimg.com/vi/7ebZWviUfUA/maxresdefault.jpg";
     this.ingredient = [new Ingredient("Egg", "2"), new Ingredient("Milk", "5 dl")];
     this.description = "This is a very good recipe. Eat pancakes.";
+    this.category = "Dairy";
+    this.instruction = "Mix everything well and then fry it in a pan";
   }
 }
