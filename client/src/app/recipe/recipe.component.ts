a2b894f6dfa6f486108c53dc8c0dbf9385f56b17
SofHay <sofhay@live.se>
2018-02-02 12:53:37 +0100

    hiii

--- client/src/app/recipe/recipe.component.ts
+++ client/src/app/recipe/recipe.component.ts
@@ -1,7 +1,9 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from "../models/recipe.modal";
 import {Ingredient} from "../models/ingredient.modal";
-
+import {RecipeService} from "../services/recipe.service";
+import {Comment}from "../models/comment.modal";
+import {Rating}from "../models/rating.modal";
 
 @Component({
 
@@ -14,48 +16,57 @@ import {Ingredient} from "../models/ingredient.modal";
 
 export class RecipeComponent implements OnInit {
   @Input() rating: number;
-  @Input() submitComment: number;
-  @Input() itemId: number;
   @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
 
 
+  comment: Comment;
+  commentList: Comment[];
+
+  rating: Rating;
   recipe: Recipe;
   ingredient: Ingredient[];
 
 
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
+    this.comment = new Comment();
+    this.rating = new Rating();
 
     this.ingredient = [
       new Ingredient('Bread', '1 slice'),
       new Ingredient('Bacon', '2 slices')
     ];
+    this.commentList = [
+      {name: "Namn", comment: "Comment"},
+
+    ];
+
     this.recipe = new Recipe;
     this.recipe.imgURL = 'https://d1doqjmisr497k.cloudfront.net/-/media/schwartz/recipes/800x800/bacon_sandwich_800.ashx?vd=20160802T004146Z&ir=1&width=600&height=600&crop=auto&quality=75&speed=0&hash=E34ACE89265017EAE6FA80D499960D876B388F200';
-    this.recipe.ingredient
     this.recipe.title = 'Title';
     this.recipe.description = 'Work, work, work!';
+
 
   }
 
 
   ngOnInit() {
-    console.log(this.recipe);
+
   }
 
   onClick(rating: number): void {
     this.rating = rating;
     this.ratingClick.emit({
-      itemId: this.itemId,
       rating: rating
     });
-
+  this.recipeService.saveCRatingToDatabase(this.rating);
   }
-  onClick(submitComment: number): void {
-    this.submitComment = submitComment;
-    console.log("yay")
-
-
+  registerComment(name: string , comment: string){
+    console.log(this.comment);
+    this.recipeService.saveCommentToDatabase(this.comment);
+    this.commentList = [
+      {name,comment}
+    ]
   }
 }
 
--- client/src/app/services/recipe.service.ts
+++ client/src/app/services/recipe.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Comment} from "../models/comment.modal";
+import {Rating} from "../models/rating.modal";
 
 @Injectable()
 export class RecipeService {
 
   constructor() { }
-
+  saveCommentToDatabase(comment: Comment) {
+    console.log(comment);
+  }
+  saveCRatingToDatabase(rating: Rating) {
+    console.log(rating);
+  }
 }
