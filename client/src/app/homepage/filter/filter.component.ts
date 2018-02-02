f14089f72782ee2d747cd08fd41cd485f2124189
M3832 <markus3832@gmail.com>
2018-02-02 14:43:18 +0100

    Updated some styles in the front page, added a search- and filterbar

---
+++ client/src/app/homepage/filter/filter.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent implements OnInit {
+
+  categories: string [];
+
+  constructor() {
+    this.categories = new Array();
+  }
+
+  ngOnInit() {
+    this.setCategories();
+    console.log(this.categories);
+  }
+
+
+  setCategories() {
+    this.categories.push("Dairy", "Vegan", "Meat", "Fish", "Poultry");
+  }
+
+}
