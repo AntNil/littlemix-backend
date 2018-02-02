a5d5ee5f8b1a2b7a35b92f3adbe748ccb9669992
JenGisslow <gisslow_jenni@hotmail.com>
2018-02-02 11:32:48 +0100

    read all the entities by a specific id is now working. we will have to create more read-methods as we go

--- src/main/java/com/littlemixrecipes/littlemix/LittlemixApplication.java
+++ src/main/java/com/littlemixrecipes/littlemix/LittlemixApplication.java
@@ -4,6 +4,7 @@ import com.littlemixrecipes.littlemix.entities.*;
 import com.littlemixrecipes.littlemix.services.CreateEntity;
 import com.littlemixrecipes.littlemix.services.DeleteEntity;
 import com.littlemixrecipes.littlemix.services.ReadEntity;
+import com.littlemixrecipes.littlemix.services.UpdateEntity;
 import com.littlemixrecipes.littlemix.services.repositories.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -23,16 +24,21 @@ public class LittlemixApplication {
 	public CommandLineRunner demo(RecipeRepository recipeRepository, IngredientsRepository ingredientsRepository, CommentRepository commentRepository, GradeRepository gradeRepository, UserRepository userRepository) {
 		return (args) -> {
 
-			//All the create's working!
-/*
+
 			//Objects
 			CreateEntity createEntity = new CreateEntity();
+			ReadEntity readEntity = new ReadEntity();
+			DeleteEntity deleteEntity = new DeleteEntity();
+			UpdateEntity updateEntity = new UpdateEntity();
+
 			RecipeEntity recipeEntity = new RecipeEntity();
 			IngredientsEntity ingredientsEntity = new IngredientsEntity();
 			CommentEntity commentEntity = new CommentEntity();
 			GradeEntity gradeEntity = new GradeEntity();
 			UserEntity userEntity = new UserEntity();
 
+			//All the create's working!
+/*
 			//Create user data
 			userEntity.setFirstName("Jennifer");
 			userEntity.setLastName("Gisslow");
@@ -78,16 +84,60 @@ public class LittlemixApplication {
 
 			*/
 
-/*
-			//Read the different entities
-			ReadEntity readEntity = new ReadEntity();
-			RecipeEntity recipe = readEntity.readRecipe(repository, 1);
-			System.out.println(recipe.getUserName() + " DET FUNKAR !!!!!!!!!!!");
+			//All the read by id is working!
+			/*
+
+			//Read user;
+			userEntity = readEntity.readUser(userRepository, 1);
+			System.out.println(userEntity.getFirstName() + " DET FUNKAR !!!!!!!!!!!");
 			System.out.println("//////////////////");
 
-			//Delete the different entities
+			//Read Recipe
+			recipeEntity = readEntity.readRecipe(recipeRepository, 1);
+			System.out.println(recipeEntity.getRecipeTitle() + " DET FUNKAR !!!!!!!!!!!");
+			System.out.println("//////////////////");
+
+
+			//Read ingredients
+			ingredientsEntity = readEntity.readIngredients(ingredientsRepository, 1);
+			System.out.println(ingredientsEntity.getIngredientName() + " DET FUNKAR !!!!!!!!!!!");
+			System.out.println("//////////////////");
+
+			//Read comments
+			commentEntity = readEntity.readComments(commentRepository, 1);
+			System.out.println(commentEntity.getCommentText() + " DET FUNKAR !!!!!!!!!!!");
+			System.out.println("//////////////////");
+
+			//Read Grade
+			gradeEntity = readEntity.readGrade(gradeRepository, 1);
+			System.out.println(gradeEntity.getGradePoints() + " DET FUNKAR !!!!!!!!!!!");
+			System.out.println("//////////////////");
+
+			*/
+
+			//Update user
+
+			//Update recipe
+
+			//Update ingredients
+
+			//Update comment
+
+			//Update grade
+
+
+			/*
+			//Delete user
+
+			//Delete recipe
 			DeleteEntity deleteEntity = new DeleteEntity();
 			deleteEntity.deleteRecipe(repository, 2);
+
+			//Delete ingredients
+
+			//Delete comment
+
+			//Delete grade
 			*/
 		};
 	}
--- src/main/java/com/littlemixrecipes/littlemix/services/ReadEntity.java
+++ src/main/java/com/littlemixrecipes/littlemix/services/ReadEntity.java
@@ -1,33 +1,37 @@
 package com.littlemixrecipes.littlemix.services;
 
 import com.littlemixrecipes.littlemix.entities.*;
-import com.littlemixrecipes.littlemix.services.repositories.RecipeRepository;
+import com.littlemixrecipes.littlemix.services.repositories.*;
 
 /**
  * Created by jennifergisslow on 2018-02-01.
  */
 public class ReadEntity {
 
+    //Read one with a specific ID, lets create some more reads as we go ..
+
     public RecipeEntity readRecipe(RecipeRepository repository, int recipeId){
-
         RecipeEntity recipe = repository.findOne(recipeId);
-
         return recipe;
     }
 
-    public IngredientsEntity readIngredients(){
-        return null;
+    public IngredientsEntity readIngredients(IngredientsRepository repository, int ingredientsId){
+        IngredientsEntity ingredientsEntity = repository.findOne(ingredientsId);
+        return ingredientsEntity;
     }
 
-    public CommentEntity readComments(){
-        return null;
+    public CommentEntity readComments(CommentRepository repository, int commentId){
+        CommentEntity commentEntity = repository.findOne(commentId);
+        return commentEntity;
     }
 
-    public GradeEntity readGrade(){
-        return null;
+    public GradeEntity readGrade(GradeRepository repository, int gradeId){
+        GradeEntity gradeEntity = repository.findOne(gradeId);
+        return gradeEntity;
     }
 
-    public UserEntity readUser(){
-        return null;
+    public UserEntity readUser(UserRepository repository, int userId){
+        UserEntity userEntity = repository.findOne(userId);
+        return userEntity;
     }
 }
