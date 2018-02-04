7b4b4e373f3d4d342be115caa7240cc7fb2f4b7e
JenGisslow <gisslow_jenni@hotmail.com>
2018-02-04 14:40:28 +0100

    added a new controller for the comments and a new repository method for the comments. it's now possible to read, create, update and delete a comment. Tested with postman

---
+++ src/main/java/com/littlemixrecipes/littlemix/controllers/CommentController.java
@@ -0,0 +1,57 @@
+package com.littlemixrecipes.littlemix.controllers;
+
+import com.littlemixrecipes.littlemix.entities.CommentEntity;
+import com.littlemixrecipes.littlemix.services.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*Created by jennifergisslow on 2018-02-04.*/
+
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentController(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/getComments")
+    public List<CommentEntity> getAllCommentsForARecipeById(@RequestParam int recipeId){
+        return (List<CommentEntity>) commentRepository.findByRecipeId(recipeId);
+    }
+
+    @PostMapping("/createComment")
+    public CommentEntity createAComment(@RequestBody CommentEntity commentObject){
+        return commentRepository.save(commentObject);
+    }
+
+    @PutMapping("/updateComment")
+    public ResponseEntity<CommentEntity> updateComment(@RequestParam int commentId, @RequestBody CommentEntity commentObject){
+        CommentEntity commentToChange = commentRepository.findOne(commentId);
+        if (commentToChange == null){
+            return new ResponseEntity<CommentEntity>(HttpStatus.NOT_FOUND);
+        }
+        commentToChange.setCommentText(commentObject.getCommentText());
+        commentToChange.setCommentDate(commentObject.getCommentDate());
+        commentToChange.setUserName(commentObject.getUserName());
+        commentToChange.setRecipeId(commentObject.getRecipeId());
+
+        CommentEntity updatedComment = commentRepository.save(commentToChange);
+        return new ResponseEntity<CommentEntity>(updatedComment, HttpStatus.OK);
+    }
+
+    @DeleteMapping("deleteComment")
+    public void deleteCommentById(@RequestParam int commentId){
+        commentRepository.delete(commentId);
+    }
+
+}
--- src/main/java/com/littlemixrecipes/littlemix/services/repositories/CommentRepository.java
+++ src/main/java/com/littlemixrecipes/littlemix/services/repositories/CommentRepository.java
@@ -2,8 +2,16 @@ package com.littlemixrecipes.littlemix.services.repositories;
 
 
 import com.littlemixrecipes.littlemix.entities.CommentEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends CrudRepository<CommentEntity, Integer> {
+
+    @Query("Select c from CommentEntity c where c.recipeId =:recipeId")
+    List<CommentEntity> findByRecipeId(@Param("recipeId") int recipeId);
+
 
 }
