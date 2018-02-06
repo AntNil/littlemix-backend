419dae4c779ac9efd799f263f4abd5e3950ef12e
Sovsa <sovsa74@gmail.com>
2018-02-06 14:03:27 +0100

    Forgot to save in past commit

--- src/main/java/com/littlemixrecipes/littlemix/controllers/RecipeController.java
+++ src/main/java/com/littlemixrecipes/littlemix/controllers/RecipeController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> cfbc1f98cc49cbd8c3ef63edcd8f08e87a52ce69
 
 import com.littlemixrecipes.littlemix.entities.RecipeEntity;
 
