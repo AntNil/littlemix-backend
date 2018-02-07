f1f3fecab5f285669f92e972f360992c1b0c626d
markus3832@gmail.com <markus3832@gmail.com>
2018-02-07 13:16:53 +0100

    Register user

--- src/main/java/com/littlemixrecipes/littlemix/controllers/RecipeController.java
+++ src/main/java/com/littlemixrecipes/littlemix/controllers/RecipeController.java
@@ -55,7 +55,7 @@ public class RecipeController {
 		});
 		return new ResponseEntity<List<RecipeEntity>>(recipeList, HttpStatus.OK);
 	}
-	
+
 	@PutMapping(path="/update")
 	public ResponseEntity<RecipeEntity> updateRecipe(@RequestBody RecipeEntity recipeModel){
 		int currentRecipeId = recipeModel.getRecipeId();
--- src/main/java/com/littlemixrecipes/littlemix/controllers/UserController.java
+++ src/main/java/com/littlemixrecipes/littlemix/controllers/UserController.java
@@ -4,17 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.littlemixrecipes.littlemix.entities.UserEntity;
 import com.littlemixrecipes.littlemix.services.UserRepository;
 
 @Controller
+@CrossOrigin
 @RequestMapping(path="/user")
 public class UserController {
 	@Autowired
