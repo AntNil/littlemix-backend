d3e28bba932c0ace68bb81e80e08e1e9da54e31d
SofHay <sofhay@live.se>
2018-02-08 13:13:10 +0100

    update

--- client/src/app/homepage/filter/filter.component.ts
+++ client/src/app/homepage/filter/filter.component.ts
@@ -27,12 +27,15 @@ export class FilterComponent implements OnInit {
   }
 
   searchButtonPressed(value) {
-    this.recipes.length = 0;
+    if(!value){}
+    else {
+      this.recipes.length = 0;
 
-  this.recipeService.findPerRecipeTitle(value).then(res => {
-    var tempRecipes = res as Array<Recipe>;
-    tempRecipes.forEach(recipe => this.recipes.push(recipe));
-  });
+      this.recipeService.findPerRecipeTitle(value).then(res => {
+        var tempRecipes = res as Array<Recipe>;
+        tempRecipes.forEach(recipe => this.recipes.push(recipe));
+      });
+    }
   }
 
   select(value){
