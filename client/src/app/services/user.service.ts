89ec8b4004b191c06353d0166af36437b79c0709
Rasmus Akerstrom <rakerstroms@gmail.com>
2018-02-08 10:01:53 +0100

    login fix

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import {User} from "../models/user.modal";
 import {HttpClient} from "@angular/common/http";
+import {Params, Router, RouterLink} from "@angular/router";
+
 
 @Injectable()
 export class UserService {
 
-  constructor(private http:HttpClient) { }
+  user: User;
+  currentUser : User;
+
+  constructor(private http:HttpClient, private router: Router) {
+
+  }
 
   saveUserToDatabase(user: User) {
     this.http.post('http://localhost:8080/user/create', user).subscribe(data => {
@@ -13,7 +20,27 @@ export class UserService {
     });
   }
 
-  loginWithUser(email: string, password: string) {
-    console.log("email = " + email + ", Password = " + password);
+  loginWithUser(user: User): any {
+   return this.http.post('http://localhost:8080/user/checkLoginDetailsForUser/', user);
+    /*  this.user = data as User;
+      localStorage.setItem('currentUser', JSON.stringify(this.user));
+      this.router.navigate(['/home']);
+      return data;
+    },()=>{
+      return data;
+    }, ()=>{
+
+    })*/
+
   }
+
+  getCurrentUser() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if(this.currentUser){
+      return this.currentUser;
+    }
+    return null;
+  }
+
+
 }
